95985d8f3e8b18a2ce1cbcdf4b48350f15e31d9e
rabieHs <rabiehoussaini@gmail.com>
2023-08-13 16:29:19 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by rabie houssaini on 13/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
